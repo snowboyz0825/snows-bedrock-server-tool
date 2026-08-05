d95258732222a13d2f89e6016d4c4a14bdf269d2
snowboyz0825 <101284660+snowboyz0825@users.noreply.github.com>
2026-08-05 14:55:05 -0700

    get the stuff we care about

---
+++ install.sh
@@ -0,0 +1,9 @@
+wget https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-1.26.40.8.zip -O bedrock-server.zip
+
+unzip bedrock-server.zip -d bedrock-server
+
+rm bedrock-server.zip
+
+mkdir joinbot
+
+wget https://github.com/MCXboxBroadcast/Broadcaster/releases/download/149/MCXboxBroadcastStandalone.jar -P joinbot
